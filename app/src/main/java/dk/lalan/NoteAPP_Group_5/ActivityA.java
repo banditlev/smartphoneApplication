0368feef4583d5ee5812da14ce3b5cc55ae58552
Laurits Langberg <laurits.langberg@gmail.com>
2015-09-14 11:09:27 +0200

    H3 init

--- app/src/main/java/dk/lalan/NoteAPP_Group_5/ActivityA.java
+++ app/src/main/java/dk/lalan/NoteAPP_Group_5/ActivityA.java
@@ -1,38 +1,22 @@
-package dk.lalan.backgroundapp_group_5;
+package dk.lalan.NoteAPP_Group_5;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
 
-public class ActivityB extends AppCompatActivity {
+public class ActivityA extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_b);
-        Intent intent = getIntent();
-        String message = intent.getStringExtra("message");
-        TextView messageView = (TextView) findViewById(R.id.messageView);
-        messageView.setText(message);
-
-        Button backButton = (Button) findViewById(R.id.backButton);
-        backButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                finish();
-            }
-        });
+        setContentView(R.layout.activity_a);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_activity_b, menu);
+        getMenuInflater().inflate(R.menu.menu_a, menu);
         return true;
     }
 
@@ -50,5 +34,4 @@ public class ActivityB extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
 }
