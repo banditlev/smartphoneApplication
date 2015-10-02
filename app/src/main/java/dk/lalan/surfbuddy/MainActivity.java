975be2d2ae3dab84336421b526a25d2bf391eac7
banditlev <sditlev@hotmail.com>
2015-10-02 14:57:18 +0200

    Custom menu implementation

--- app/src/main/java/dk/lalan/surfbuddy/MainActivity.java
+++ app/src/main/java/dk/lalan/surfbuddy/MainActivity.java
@@ -1,11 +1,11 @@
 package dk.lalan.surfbuddy;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,18 +20,4 @@ public class MainActivity extends AppCompatActivity {
         return true;
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- app/src/main/java/models/SurfLocation.java
+++ app/src/main/java/models/SurfLocation.java
@@ -1,3 +1,5 @@
+package models;
+
 /**
  * Created by lalan on 02/10/15.
  */
