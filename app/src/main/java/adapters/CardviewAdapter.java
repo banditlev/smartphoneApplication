16dc66813aee2738a27428e2538990a6353d564f
banditlev <sditlev@hotmail.com>
2015-10-03 10:43:54 +0200

    Button implementation on cards

--- app/src/main/java/adapters/CardviewAdapter.java
+++ app/src/main/java/adapters/CardviewAdapter.java
@@ -12,8 +12,10 @@ import android.view.LayoutInflater;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.List;
 
@@ -46,29 +48,44 @@ public class CardviewAdapter extends RecyclerView.Adapter<CardviewAdapter.ViewHo
     @Override
     public void onBindViewHolder(ViewHolder viewHolder, int i){
         DummySurfer favorite = favorites.get(i);
-
+        final int row = i;
         viewHolder.favoriteName.setText(favorite.getName());
         viewHolder.favoriteDescription.setText(favorite.getDescription());
         viewHolder.favoriteWindDirection.setText(favorite.getWindDirection());
         viewHolder.favoriteTemp.setText(Double.toString(favorite.getTemp()) + "º C");
         viewHolder.favoriteWindSpeed.setText(Double.toString(favorite.getWindSpeed()) + " knots");
 
+
         //If surfable use green arrow and ring else use red
-        //Inspired by: http://www.learn-android-easily.com/2013/07/imageview-animation-in-android.html
         if(favorite.isSurfable()){
-            RotateAnimation animRing = new RotateAnimation(0, 180, 120, 120);
-            RotateAnimation animArrow = new RotateAnimation(0, -130, 60, 60);
-            animRing.setInterpolator(new LinearInterpolator());
-            animArrow.setInterpolator(new LinearInterpolator());
-            animRing.setFillAfter(true);
-            animArrow.setFillAfter(true);
-            animRing.setDuration(2000);
-            animArrow.setDuration(2000);
+            //Start animation on imageviews
             viewHolder.favoriteWindDirectionRing.setImageDrawable(context.getDrawable(R.mipmap.ic_direction_green));
             viewHolder.favoriteWindDirectionArrow.setImageDrawable(context.getDrawable(R.mipmap.ic_arrow_green));
-            viewHolder.favoriteWindDirectionArrow.startAnimation(animArrow);
-            viewHolder.favoriteWindDirectionRing.startAnimation(animRing);
+        }else{
+            /*
+            TODO:implement with correct image location
+            viewHolder.favoriteWindDirectionRing.setImageDrawable(context.getDrawable(R.mipmap.ic_direction_red));
+            viewHolder.favoriteWindDirectionArrow.setImageDrawable(context.getDrawable(R.mipmap.ic_arrow_red));
+            */
         }
+
+        //Set up button listeners for each card
+        viewHolder.cardviewNavigationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Button click Nav " + row, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        viewHolder.cardviewMoreInfoButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(context, "Button click More " + row, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        animateIcons(viewHolder, favorite);
+
     }
 
     @Override
@@ -77,7 +94,7 @@ public class CardviewAdapter extends RecyclerView.Adapter<CardviewAdapter.ViewHo
     }
 
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        public TextView favoriteName, favoriteDescription, favoriteWindDirection, favoriteWindSpeed, favoriteTemp;
+        public TextView favoriteName, favoriteDescription, favoriteWindDirection, favoriteWindSpeed, favoriteTemp, cardviewNavigationButton, cardviewMoreInfoButton;
         public ImageView favoriteWindDirectionRing, favoriteWindDirectionArrow;
 
         public ViewHolder(View itemView) {
@@ -89,7 +106,27 @@ public class CardviewAdapter extends RecyclerView.Adapter<CardviewAdapter.ViewHo
             favoriteTemp = (TextView) itemView.findViewById(R.id.favoriteTemp);
             favoriteWindDirectionRing = (ImageView) itemView.findViewById(R.id.favoriteWindDirectionRing);
             favoriteWindDirectionArrow = (ImageView) itemView.findViewById(R.id.favoriteWindDirectionArrow);
+            cardviewMoreInfoButton = (TextView) itemView.findViewById(R.id.carviewMoreInfoButton);
+            cardviewNavigationButton = (TextView) itemView.findViewById(R.id.cardviewNavigateButton);
         }
 
     }
+
+    //Do animation of rotating both pictures to sesired angle from 0
+    //Inspired by: http://www.learn-android-easily.com/2013/07/imageview-animation-in-android.html
+    //animation setup for both icons
+    public void animateIcons(ViewHolder viewHolder, DummySurfer favorite){
+        RotateAnimation animRing = new RotateAnimation(0, 180, 120, 120);
+        RotateAnimation animArrow = new RotateAnimation(0, -130, 60, 60);
+        animRing.setInterpolator(new LinearInterpolator());
+        animArrow.setInterpolator(new LinearInterpolator());
+        animRing.setFillAfter(true);
+        animArrow.setFillAfter(true);
+        animRing.setDuration(2000);
+        animArrow.setDuration(2000);
+
+        viewHolder.favoriteWindDirectionArrow.startAnimation(animArrow);
+        viewHolder.favoriteWindDirectionRing.startAnimation(animRing);
+    }
 }
+
