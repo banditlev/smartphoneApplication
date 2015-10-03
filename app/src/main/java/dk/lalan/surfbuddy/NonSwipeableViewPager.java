877fdf906323927c095ef6221584b2916c29f095
Søren Lundtoft <soren.lundtoft@gmail.com>
2015-10-03 23:34:51 +0200

    Removed swiping

---
+++ app/src/main/java/dk/lalan/surfbuddy/NonSwipeableViewPager.java
@@ -0,0 +1,31 @@
+package dk.lalan.surfbuddy;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Inspired from: http://stackoverflow.com/questions/9650265/how-do-disable-paging-by-swiping-with-finger-in-viewpager-but-still-be-able-to-s
+ * Created by lundtoft on 03/10/15.
+ */
+public class NonSwipeableViewPager extends ViewPager {
+
+    public NonSwipeableViewPager(Context context) {
+        super(context);
+    }
+
+    public NonSwipeableViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent event) {
+        return false;
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return false;
+    }
+}
