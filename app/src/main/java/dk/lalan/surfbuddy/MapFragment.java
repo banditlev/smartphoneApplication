6a382d327317378026ce07a076f7679446271c33
Søren Lundtoft <soren.lundtoft@gmail.com>
2015-10-04 16:30:39 +0200

    Map now uses gps

--- app/src/main/java/dk/lalan/surfbuddy/MapFragment.java
+++ app/src/main/java/dk/lalan/surfbuddy/MapFragment.java
@@ -5,8 +5,13 @@ import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.drawable.BitmapDrawable;
 import android.graphics.drawable.Drawable;
+import android.location.Criteria;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -24,34 +29,73 @@ import java.util.GregorianCalendar;
 import java.util.LinkedList;
 
 
-public class MapFragment extends Fragment {
+public class MapFragment extends Fragment implements LocationListener {
 
     private DefaultResourceProxyImpl resourceProxy;
     private ArrayList<OverlayItem> items = new ArrayList<>();
+    private LocationManager locationManager;
+    private String provider;
+    private Location location;
+    private MapView map;
+    private SurfLocation surfLocation;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_map, container, false);
 
-        MapView map = (MapView) view.findViewById(R.id.mapview);
-        SurfLocation surfLocation = ((LocationInformationActivity) getActivity()).getSurfLocation();
-
+        map = (MapView) view.findViewById(R.id.mapview);
+        surfLocation = ((LocationInformationActivity) getActivity()).getSurfLocation();
         GeoPoint surfPoint = new GeoPoint(surfLocation.getlatitude(), surfLocation.getLongitude());
-        GeoPoint gpsPoint = new GeoPoint(56.147850, 10.200130);
+
+        //Init marker list
+        items = new ArrayList<>();
+        resourceProxy = new DefaultResourceProxyImpl(view.getContext());
+
+        //Inspired from: http://www.vogella.com/tutorials/AndroidDrawables/article.html
+        locationManager = (LocationManager) view.getContext().getSystemService(Context.LOCATION_SERVICE);
+        Criteria criteria = new Criteria();
+        provider = locationManager.getBestProvider(criteria, false);
+        location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+            onLocationChanged(location);
+        }
 
         map.setBuiltInZoomControls(false);
         map.setMultiTouchControls(true);
         map.getController().setZoom(12);
         map.getController().setCenter(surfPoint);
 
-        //Init marker list
-        items = new ArrayList<>();
-        resourceProxy = new DefaultResourceProxyImpl(view.getContext());
-        addMarker(gpsPoint, this.getResources().getDrawable(R.drawable.gps), map);                                                                          
         addMarker(surfPoint, this.getResources().getDrawable(R.drawable.marker), map);
 
-
         return view;
+    }
+
+    @Override
+    public void onLocationChanged(Location loc) {
+        if(map != null) {
+            items.clear();
+
+            GeoPoint gpsPoint = new GeoPoint(loc.getLatitude(), loc.getLongitude());
+            GeoPoint surfPoint = new GeoPoint(surfLocation.getlatitude(), surfLocation.getLongitude());
+            addMarker(gpsPoint, this.getResources().getDrawable(R.drawable.gps), map);
+            addMarker(surfPoint, this.getResources().getDrawable(R.drawable.marker), map);
+        }
+    }
+
+    @Override
+    public void onStatusChanged(String provider, int status, Bundle extras) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
     }
 
     /**
